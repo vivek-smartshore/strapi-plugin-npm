e32d54bb71f9ea2510cc12c4afa139cbad1ba59a
vivek-smartshore <vivek@smartshore.nl>
2025-04-08 15:01:54 +0530

    version 4

--- server/register.ts
+++ server/register.ts
@@ -2,71 +2,74 @@ import { Strapi } from '@strapi/strapi'
 import { FBContentTypes } from './types'
 import { disableAdminOnlyFieldsGraphQL } from './utils'
 
-export default async ({ strapi }: { strapi: Strapi }) => {
-  if (strapi.plugin('graphql')) {
-    const extensionService = strapi.plugin('graphql').service('extension')
+export default ({ strapi }: { strapi: Strapi }) => {
+  const graphqlPlugin = strapi.plugin('graphql')
+  if (!graphqlPlugin) return
 
-    // Disable admin only fields for Forms and Submissions
-    disableAdminOnlyFieldsGraphQL(extensionService, FBContentTypes.FB_FORMS)
-    disableAdminOnlyFieldsGraphQL(extensionService, FBContentTypes.FB_SUBMISSIONS)
+  const extensionService = graphqlPlugin.service('extension')
 
-    extensionService.use({
-      typeDefs: `
-        type Mutation {
-          createFormBuilderSubmission(data: CreateSubmissionInput!): FormBuilderSubmissionEntityResponse
-        }
+  // Disable admin only fields for Forms
+  disableAdminOnlyFieldsGraphQL(extensionService, FBContentTypes.FB_FORMS)
 
-        input FormFieldInput {
-          id: String!
-          value: JSON
-        }
+  // Disable admin only fields for Submissions
+  disableAdminOnlyFieldsGraphQL(extensionService, FBContentTypes.FB_SUBMISSIONS)
 
-        input CreateSubmissionInput {
-          submittedData: [FormFieldInput!]!
-          form: ID!
-        }
-      `,
-      resolvers: {
-        Mutation: {
-          createFormBuilderSubmission: {
-            resolve: async (
-              _: any,
-              args: {
-                data: {
-                  submittedData: { id: string; value: any }[]
-                  form: string | number
-                }
+  extensionService.use({
+    typeDefs: `
+      type Mutation {
+        createFormBuilderSubmission(data: CreateSubmissionInput!): FormBuilderSubmissionEntityResponse
+      }
+
+      input FormFieldInput {
+        id: String!
+        value: JSON
+      }
+
+      input CreateSubmissionInput {
+        submittedData: [FormFieldInput!]!
+        form: ID!
+      }
+    `,
+    resolvers: {
+      Mutation: {
+        createFormBuilderSubmission: {
+          resolve: async (
+            _: any,
+            args: {
+              data: {
+                submittedData: { id: string; value: any }[]
+                form: string | number
               }
-            ) => {
-              const filteredSubmittedData = args.data.submittedData.filter(
-                (field) => field.value !== undefined && field.value !== null
-              )
+            }
+          ) => {
+            const filteredSubmittedData = args.data.submittedData.filter(
+              (field) => field.value !== undefined && field.value !== null
+            )
 
-              const submittedData = filteredSubmittedData.reduce((acc, field) => {
-                acc[field.id] = field.value
-                return acc
-              }, {} as Record<string, any>)
+            const submittedData = filteredSubmittedData.reduce((acc, field) => {
+              acc[field.id] = field.value
+              return acc
+            }, {} as Record<string, any>)
 
-              const formId = typeof args.data.form === 'string' ? parseInt(args.data.form, 10) : args.data.form
+            const formId = typeof args.data.form === 'string' ? parseInt(args.data.form, 10) : args.data.form
 
-              const submission = await strapi
-                .plugin('form-builder')
-                .service('submission')
-                .create({
-                  data: {
-                    submittedData,
-                    form: formId,
-                  },
-                })
+            const submission = await strapi
+              .plugin('form-builder')
+              .service('submission')
+              .create({
+                data: {
+                  submittedData,
+                  form: formId,
+                },
+              })
 
-              const { toEntityResponse } = strapi.service('plugin::graphql.format').returnTypes
-              return toEntityResponse(submission.results)
-            },
+            const { toEntityResponse } = strapi.service('plugin::graphql.format').returnTypes
+            return toEntityResponse(submission.results)
           },
         },
       },
-    })
-  }
+    },
+  })
 
   strapi.customFields.register({
     name: 'form-relation-id',
