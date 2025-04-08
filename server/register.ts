9681412c7c8a67b27fc58780e510b180370fcdce
vivek-smartshore <vivek@smartshore.nl>
2025-04-08 14:01:25 +0530

    version 3

--- server/register.ts
+++ server/register.ts
@@ -2,31 +2,14 @@ import { Strapi } from '@strapi/strapi'
 import { FBContentTypes } from './types'
 import { disableAdminOnlyFieldsGraphQL } from './utils'
 
-export default ({ strapi }: { strapi: Strapi }) => {
-  // 1. Verify GraphQL plugin is installed and enabled
-  if (!strapi.plugin('graphql')) {
-    strapi.log.warn('GraphQL plugin not found - some Form Builder features may not work')
-    return
-  }
-
-  try {
+export default async ({ strapi }: { strapi: Strapi }) => {
+  if (strapi.plugin('graphql')) {
     const extensionService = strapi.plugin('graphql').service('extension')
-    if (!extensionService) {
-      throw new Error('GraphQL extension service not found')
-    }
 
-    // 2. Register custom field first (less likely to depend on other services)
-    strapi.customFields.register({
-      name: 'form-relation-id',
-      plugin: 'form-builder',
-      type: 'integer',
-    })
-
-    // 3. Disable admin only fields
+    // Disable admin only fields for Forms and Submissions
     disableAdminOnlyFieldsGraphQL(extensionService, FBContentTypes.FB_FORMS)
     disableAdminOnlyFieldsGraphQL(extensionService, FBContentTypes.FB_SUBMISSIONS)
 
-    // 4. Extend GraphQL schema
     extensionService.use({
       typeDefs: `
         type Mutation {
@@ -55,17 +38,6 @@ export default ({ strapi }: { strapi: Strapi }) => {
                 }
               }
             ) => {
-              // Verify form-builder services are available
-              const submissionService = strapi.plugin('form-builder').service('submission')
-              if (!submissionService) {
-                throw new Error('Form Builder submission service not found')
-              }
-
-              const graphqlFormatService = strapi.service('plugin::graphql.format')
-              if (!graphqlFormatService) {
-                throw new Error('GraphQL format service not found')
-              }
-
               const filteredSubmittedData = args.data.submittedData.filter(
                 (field) => field.value !== undefined && field.value !== null
               )
@@ -77,22 +49,28 @@ export default ({ strapi }: { strapi: Strapi }) => {
 
               const formId = typeof args.data.form === 'string' ? parseInt(args.data.form, 10) : args.data.form
 
-              const submission = await submissionService.create({
-                data: {
-                  submittedData,
-                  form: formId,
-                },
-              })
+              const submission = await strapi
+                .plugin('form-builder')
+                .service('submission')
+                .create({
+                  data: {
+                    submittedData,
+                    form: formId,
+                  },
+                })
 
-              const { toEntityResponse } = graphqlFormatService.returnTypes
+              const { toEntityResponse } = strapi.service('plugin::graphql.format').returnTypes
               return toEntityResponse(submission.results)
             },
           },
         },
       },
     })
-  } catch (error) {
-    strapi.log.error('Error registering Form Builder plugin:')
-    strapi.log.error(error)
   }
+
+  strapi.customFields.register({
+    name: 'form-relation-id',
+    plugin: 'form-builder',
+    type: 'integer',
+  })
 }
